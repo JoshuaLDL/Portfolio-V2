2705ba9f2a8d065937df9a171a9a0fd208238448
Joshua Downton-Lewis <joshuadowntonlewis@outlook.com>
2025-04-04 11:16:00 +0100

    import issue on image.

--- src/Components/ImageOne/index.jsx
+++ src/Components/ImageOne/index.jsx
@@ -4,7 +4,7 @@ import "./ImageOne.css"
 export default function ImageOne() {
     return (
         <div className="imageBox">
-            <img src="public/Assets/ImageOne.png" alt="photo of gumbo in a white bowl with some white rice." width="80%" height="auto" />
+            <img src="Assets/ImageOne.png" alt="photo of gumbo in a white bowl with some white rice." width="80%" height="auto" />
         </div>
     )
 
